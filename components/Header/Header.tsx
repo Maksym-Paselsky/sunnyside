42f0fac3482930fe7c739848f63be61d7f24fc08
KOMA TOZ <43058251+Maksym-Paselsky@users.noreply.github.com>
2023-08-27 17:31:55 +0300

    asssets and header component

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -2,12 +2,26 @@ import styles from "./Header.module.scss";
 export default function Header() {
   return (
     <nav>
-      <ul>
+      <ul className={styles.list}>
         <li>
-          <a href="/">Home</a>
+          <a className={styles.link} href="/">
+            About
+          </a>
         </li>
         <li>
-          <a href="/about">About</a>
+          <a className={styles.link} href="/">
+            Services
+          </a>
+        </li>
+        <li>
+          <a className={styles.link} href="/">
+            Projects
+          </a>
+        </li>
+        <li>
+          <a className={styles.link} href="/">
+            Contact
+          </a>
         </li>
       </ul>
     </nav>
